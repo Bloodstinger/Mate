8fb11438c5f5bddd6254b0162a82b42bbbc43743
Bloodstinger <47367238+Bloodstinger@users.noreply.github.com>
2019-06-09 01:39:04 +0300

    Delete ArrayAddingIndexException.java

--- src/lecture/seven/array/list/ArrayAddingIndexException.java
+++
@@ -1,8 +0,0 @@
-package lecture.seven.array.list;
-
-public class ArrayAddingIndexException extends RuntimeException{
-
-    public ArrayAddingIndexException(String s) {
-        super(s);
-    }
-}
